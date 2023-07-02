f5fbf2dd2b4f30c832b1aed00fe6e72c6288386c
chung2code <anthdny7@gmail.com>
2023-07-03 00:25:00 +0900

    오타수정

--- src/Domain/Dto/MenuDto.java
+++ src/Domain/Dto/MenuDto.java
@@ -2,37 +2,37 @@ package Domain.Dto;
 
 public class MenuDto {
 
-	private String mene_id;
-	private String manu_name;
+	private String menu_id;
+	private String menu_name;
 	private int price;
 	
 	
 	
 	public MenuDto(String mene_id, String manu_name, int price) {
 		super();
-		this.mene_id = mene_id;
-		this.manu_name = manu_name;
+		this.menu_id = mene_id;
+		this.menu_name = manu_name;
 		this.price = price;
 	}
 
 	//toString
 	@Override
 	public String toString() {
-		return "MenuDto [mene_id=" + mene_id + ", manu_name=" + manu_name + ", price=" + price + "]";
+		return "MenuDto [mene_id=" + menu_id + ", manu_name=" + menu_name + ", price=" + price + "]";
 	}
 	
 	//getter-setter
 	public String getMene_id() {
-		return mene_id;
+		return menu_id;
 	}
 	public void setMene_id(String mene_id) {
-		this.mene_id = mene_id;
+		this.menu_id = mene_id;
 	}
 	public String getManu_name() {
-		return manu_name;
+		return menu_name;
 	}
 	public void setManu_name(String manu_name) {
-		this.manu_name = manu_name;
+		this.menu_name = manu_name;
 	}
 	public int getPrice() {
 		return price;
